7ae94042277c70ee989ad4b157ac1bf5ae585c71
oumar03 <82471862+oumar03@users.noreply.github.com>
2021-04-28 15:42:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,82 @@
+namespace SpriteKind {
+    export const Box = SpriteKind.create()
+    export const Truck = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Truck.vy = -1
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Truck.vx = -1
+})
+sprites.onCreated(SpriteKind.Box, function (sprite) {
+    sprite.setImage(assets.image`Box`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Truck.vx = 1
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Truck.vy = 1
+})
+sprites.onCreated(SpriteKind.Truck, function (sprite) {
+    sprite.x = randint(10, 15)
+})
+let Truck: Sprite = null
+Truck = sprites.create(assets.image`Truck`, SpriteKind.Truck)
+let Box1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Box)
+Box1.setPosition(20, 31)
+let Box2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Box)
+Box2.setPosition(46, 61)
+let Box3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Box)
+Box3.setPosition(46, 61)
+scene.setBackgroundColor(9)
